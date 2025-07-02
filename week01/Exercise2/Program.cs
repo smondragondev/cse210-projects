0e0cea57524ae773b81337da076adf19f6cb7d43
Sergio <sergiomondragondev@gmail.com>
2025-07-02 18:24:58 -0500

    Exercise 2 solved!

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter;
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = gradePercentage % 10;
+        string appendedSymbol = "";
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                appendedSymbol = "+";
+            }
+            else
+            {
+                appendedSymbol = "-";
+            }
+        }
+        Console.WriteLine($"Your grade is {letter}{appendedSymbol}");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, This time you don't pass. Keep praciticing!");
+        }
     }
 }

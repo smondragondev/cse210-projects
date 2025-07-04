8f21d97879d8086528ee2c010976421156fb906d
Sergio <sergiomondragondev@gmail.com>
2025-07-03 22:21:50 -0500

    Exercise 3 Completed!

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber, guessedNumber, attemptCount = 0;
+        bool stopGuessing = false;
+
+        Random randomGenerator = new Random();
+        magicNumber = randomGenerator.Next(1, 100);
+
+        while (!stopGuessing)
+        {
+            attemptCount++;
+            Console.Write("What is your guess? ");
+            guessedNumber = int.Parse(Console.ReadLine());
+
+            if (guessedNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.Write("Do you want to continue? (Yes/No) ");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "yes")
+                {
+                    stopGuessing = false;
+                }
+                else
+                {
+                    stopGuessing = true;
+                    Console.WriteLine($"You did it in {attemptCount} attempts!");
+                }
+            }
+            else if (guessedNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+            
+        }
+
     }
 }

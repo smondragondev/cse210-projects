4736b1a4612c5c74527489bd7ac57640d985a715
Sergio <sergiomondragondev@gmail.com>
2025-07-11 00:23:13 -0500

    Stubs created!

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void Display()
+    {
+
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void Load()
+    {
+
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    
+}

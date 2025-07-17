f89a78f09e304c21929d8628cbee1d5dac0a5035
Sergio <sergiomondragondev@gmail.com>
2025-07-17 09:23:57 -0500

    Implement Fraction class with basic operations and update Program to demonstrate functionality

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Fraction wholeNumber = new Fraction(5);
+        Fraction realFraction1 = new Fraction(3, 4);
+        Fraction realFraction2 = new Fraction(1, 3);
+
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(wholeNumber.GetFractionString());
+        Console.WriteLine(wholeNumber.GetDecimalValue());
+        Console.WriteLine(realFraction1.GetFractionString());
+        Console.WriteLine(realFraction1.GetDecimalValue());
+        Console.WriteLine(realFraction2.GetFractionString());
+        Console.WriteLine(realFraction2.GetDecimalValue());
     }
 }

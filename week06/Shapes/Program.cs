9f182d0368942735f4b8ad932e332aa7b1399c13
Sergio <sergiomondragondev@gmail.com>
2025-08-07 11:52:01 -0500

    Add shape classes: Circle, Rectangle, Square, and Shape with area calculation

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("red", 4);
+        Rectangle rectangle = new Rectangle("black", 4, 8);
+        Circle circle = new Circle("yellow", 4);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
+
+
     }
 }
